5a7554b684130de6f76c2c96e1d70cbc8851f49b
Amulya <amulya.adapa@gmail.com>
2022-04-29 18:56:08 +0530

    First commit

---
+++ QueueInsert/Program.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Storage.Queues;
+using System;
+
+namespace QueueInsert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateQueueClient("myqueue-items");
+            Console.WriteLine("Hello World!");
+        }
+
+        private static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static void CreateQueueClient(string queueName)
+        {
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=cvejpdev;AccountKey=pmkCPAg0Jn+POws3rISYwgjETCqk5j+/ezvZlWWKfLTB+GOnpn4LuklE1ihrlkyhAtGkEv+pvHXWq00lDchW/g==;EndpointSuffix=core.windows.net";
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+           
+            if (queueClient.Exists())
+            {
+                
+                queueClient.SendMessage(Base64Encode("Hello World!"));
+            }
+        }
+    }
+}
